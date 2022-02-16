5b5212f74e770316da04f9e9fc6eb7ddaac251b2
joshua360x <jkw5499@psu.edu>
2022-02-16 13:56:08 -0500

    added weather for states in the US + passed formdata as object in serverless

--- functions/weather.js
+++ functions/weather.js
@@ -1,12 +1,19 @@
-const request = require('superagent');
-
+const fetch = require('node-fetch');
+const querystring = require('querystring');
 require('dotenv').config();
 
 exports.handler = async (event, context) => {
   try {
+    const formData = JSON.parse(event.body);
     // grab the city, state, and country from the request's query parameters
     // here is an example from the netlify docs:
-    // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D 
+    // https://functions.netlify.com/playground/#hello%2C-%7Bname%7D
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${formData.city},${formData.state},${formData.country}&limit=&appid=8041ec40a57a39496481f160dc653911`
+    );
+
+    const json = await response.json();
+    console.log('🚀 ', json);
 
     // tragicly, we cannot just pass the city name to this API. it wants a latitude and longitude for the weather
     // consult the yelp docs to figure out how to use a city, state, and country to make a request and get the latitude and longitude
@@ -15,11 +22,10 @@ exports.handler = async (event, context) => {
     // once you have gotten the lat/lon using the geocoding api, use the lat/lon to get the weather. Consult the docs below:
     // https://openweathermap.org/api/one-call-api
 
-
-    return { 
-      statusCode: 200, 
-    // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the weather data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+    return {
+      statusCode: 200,
+      // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the weather data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
+      body: JSON.stringify(json),
     };
   } catch (error) {
     console.log(error);
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,28 +1,57 @@
 import { useState } from 'react';
+import Spinner from './Spinner';
+import StatesOption from './StatesOption';
 
 export default function WeatherSearch() {
   // you'll need to track your weather search results, the loading state, and a form field for location with a default value.
   const [isLoading, setIsLoading] = useState(false);
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    city: 'portland',
+    state: 'or',
+    country: 'usa',
+  });
+
+  console.log('🚀 ~ file: WeatherSearch.js ~ line 8 ~ WeatherSearch ~ formData', formData);
   const [weatherARR, setWeatherARR] = useState([]);
 
   async function handleWeatherSubmit(e) {
     e.preventDefault();
 
     // set the loading state to true
+    setIsLoading(true);
     // use fetch to make a request to your netlify weather function. Be sure to pass the location as a query param in the URL
 
+    console.log('form data logged', formData);
+    const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify(formData),
+    };
+
+    const response = await fetch(`/.netlify/functions/weather`, requestOptions);
+
+    const json = await response.json();
+    console.log('🚀 ~ file: WeatherSearch.js ~ line 22 ~ handleWeatherSubmit ~ json', json);
+
     // put the jsonified data in state and set the loading state to false
+    setIsLoading(false);
   }
 
   return (
     <section className="weather">
       {/* make the fetch on submit */}
       <form onSubmit={handleWeatherSubmit}>
-        Search weather for a city
+        Search weather for a city In the US Only
         {/* add inputs/labels for city name, state, and country, using all the things we need with react forms. Don't forget to use the value property to sync these up with the default values in react state */}
+        <input
+          type="text"
+          value={formData.city}
+          onChange={(e) => setFormData({ ...formData, city: e.target.value })}
+          placeholder="enter city"
+        />
+        <StatesOption formData={formData} setStateOfUnitedStatesData={setFormData} />
         <button>Get weather</button>
       </form>
+      {isLoading && <Spinner />}
       {/* Make a ForecastList component to import and use here. Use a ternery to display a loading spinner (make a <Spinner /> component for this) if the data is still loading. */}
     </section>
   );
