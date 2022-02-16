ac41a6b55ad7310f74dd14eabd63d796e816345c
joshua360x <jkw5499@psu.edu>
2022-02-15 20:28:14 -0500

    added styling to pokemon

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PokemonList({ pokemon, url_image }) {
   return (
-    <div>
+    <div className='pokemonList'>
       <p>{pokemon}</p>
       <img className='pokemon-img' src={`${url_image}`} alt={`${pokemon}`} />
     </div>
