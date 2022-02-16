96db54e870ed0b0b1e8e9a878d48a33272b2e44c
joshua360x <jkw5499@psu.edu>
2022-02-15 19:58:05 -0500

    addeed out of 5 for readability

--- src/BusinessList.js
+++ src/BusinessList.js
@@ -4,6 +4,6 @@ export default function BusinessList({ name, image_url, rating }) {
   return <div className='business'>
     <p>{name}</p>
     <img className='business-img' src={image_url} alt={name} />
-    <p>{rating}</p>
+    <p>{rating} out of 5</p>
   </div>;
 }
