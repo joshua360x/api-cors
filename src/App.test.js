3c806f4a007ff96915250caafa4ec5f3d32e6b09
joshua360x <jkw5499@psu.edu>
2022-02-18 18:19:16 -0500

    configured test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/get/i);
+  const linkElement = screen.getByText(/only/i);
   expect(linkElement).toBeInTheDocument();
 });
